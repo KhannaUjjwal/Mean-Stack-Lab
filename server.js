15892fef961087f141e3ecd98f54f43c4787419e
KhannaUjjwal <ujjwalkhanna123@gmail.com>
2021-03-09 11:21:21 +0530

    Modified

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const express = require ( 'express' );
 const app = express ();
 // set our port
 const port = 3000 ;
-app.get ( '/' , ( req , res ) => res.send ( 'Welcome to M!ean stack : Ujjwal Khanna 1805570' ));// startup our app at http://localhost:3000
+app.get ( '/' , ( req , res ) => res.send ( 'Welcome to Mean stack : Ujjwal Khanna 1805570' ));// startup our app at http://localhost:3000
 app.listen ( port , () =>console.log ( `Example app listening on port ${port}!` ));
